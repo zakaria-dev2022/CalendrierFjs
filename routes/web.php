37155b107e3a40b960528a78dd9fcaaf7db3e7c9
zakaria <zakaria.rebibi2@gmail.com>
2024-06-28 01:02:26 +0100

    creation page dashbord + controller + routes

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\calendrierController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\TachesController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+});
+
+Route::resource('clients', ClientController::class);
+Route::resource('taches', TachesController::class);
+Route::resource('calendrier', calendrierController::class);
